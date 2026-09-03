2eb2f961193745bc684358a20f41041536ebe622
anugrabhagat <anugrabhagat9@gmail.com>
2026-09-03 12:36:20 +0530

    Create List table with sample data

---
+++ exp1.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS List;
+CREATE TABLE List (
+  serial_no INT,
+item VARCHAR(20),
+price INT
+);
+INSERT INTO LIST (Serial_no, Item, Price)
+VALUES
+(1, 'BISCUIT', 100),
+(2, 'GHEE', 550),
+(3, 'CHIPS', 45);
